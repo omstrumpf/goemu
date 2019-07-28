b62634eff6623bbe9bad49aade90fc918371424e
omstrumpf <omstrumpf@gmail.com>
2019-07-27 21:58:11 -0400

    test for CPU flag operations

--- internal/app/gba/cpu_test.go
+++ internal/app/gba/cpu_test.go
@@ -20,3 +20,65 @@ func TestCPUInit(t *testing.T) {
 		t.Error("BC register should init to 0")
 	}
 }
+
+func TestCPUFlags(t *testing.T) {
+	mmu := NewMMU()
+
+	cpu := NewCPU(mmu)
+
+	if cpu.z() {
+		t.Error("Z flag should init to false")
+	}
+	if cpu.n() {
+		t.Error("N flag should init to false")
+	}
+	if cpu.h() {
+		t.Error("H flag should init to false")
+	}
+	if cpu.c() {
+		t.Error("C flag should init to false")
+	}
+
+	cpu.setZ(true)
+	if !cpu.z() {
+		t.Error("Expected Z flag to be set")
+	}
+	cpu.setN(true)
+	if !cpu.n() {
+		t.Error("Expected N flag to be set")
+	}
+	cpu.setH(true)
+	if !cpu.h() {
+		t.Error("Expected H flag to be set")
+	}
+	cpu.setC(true)
+	if !cpu.c() {
+		t.Error("Expected C flag to be set")
+	}
+
+	if cpu.AF.Lo() != 0xF0 {
+		t.Errorf("Expected flag register to match 0xF0, got %#2x", cpu.AF.Lo())
+	}
+
+	cpu.setZ(false)
+	if cpu.z() {
+		t.Error("Expected Z flag to be unset")
+	}
+	cpu.setN(false)
+	if cpu.n() {
+		t.Error("Expected N flag to be unset")
+	}
+	cpu.setH(false)
+	if cpu.h() {
+		t.Error("Expected H flag to be unset")
+	}
+	cpu.setC(false)
+	if cpu.c() {
+		t.Error("Expected C flag to be unset")
+	}
+
+	if cpu.AF.Lo() != 0x00 {
+		t.Errorf("Expected flag register to match 0x00, got %#2x", cpu.AF.Lo())
+	}
+
+}
