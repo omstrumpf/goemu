a67ae7f7362fd92caef789be6c47bc69632ecf2b
omstrumpf <omstrumpf@gmail.com>
2019-07-27 21:14:06 -0400

    implemented MMU

--- internal/app/gba/mmu.go
+++ internal/app/gba/mmu.go
@@ -1,32 +1,161 @@
 package gba
 
+import "log"
+
 // MMU represents the memory management unit.
 type MMU struct {
+	bios []byte
+	rom  []byte
+	vram []byte
+	eram []byte
+	wram []byte
+	goam []byte
+	zram []byte
+
+	zero []byte
+
+	biosEnable bool
 }
 
 // NewMMU constructs a valid MMU struct
 func NewMMU() *MMU {
 	mmu := new(MMU)
 
+	mmu.bios = make([]byte, 0x100)
+	mmu.rom = make([]byte, 0x8000)
+	mmu.vram = make([]byte, 0x8000)
+	mmu.eram = make([]byte, 0x2000)
+	mmu.wram = make([]byte, 0x2000)
+	mmu.goam = make([]byte, 0xA0)
+	mmu.zram = make([]byte, 0x80)
+
+	mmu.zero = []byte{0}
+
+	mmu.biosEnable = true
+
 	return mmu
+}
+
+// DisableBios disables the BIOS map over the main ROM
+func (mmu *MMU) DisableBios() {
+	mmu.biosEnable = false
 }
 
 // Read returns the 8-bit value from the address
 func (mmu *MMU) Read(addr uint16) byte {
-	return byte(0) // TODO
-}
-
-// Read16 returns the 16-bit value from the address
-func (mmu *MMU) Read16(addr uint16) uint16 {
-	return uint16(0) // TODO
+	buffer, offset := mmu.mmapLocation(addr)
+	return buffer[offset]
 }
 
 // Write writes the 8-bit value to the address
 func (mmu *MMU) Write(addr uint16, val byte) {
-	// TODO
+	buffer, offset := mmu.mmapLocation(addr)
+
+	// Do not write to zero buffer
+	if len(buffer) == 1 && buffer[0] == 0 {
+		return
+	}
+
+	buffer[offset] = val
+}
+
+// Read16 returns the 16-bit value from the address
+func (mmu *MMU) Read16(addr uint16) uint16 {
+	return uint16(mmu.Read(addr)) + (uint16(mmu.Read(addr+1)) << 8)
 }
 
 // Write16 writes the 16-bit value to the address
 func (mmu *MMU) Write16(addr uint16, val uint16) {
-	// TODO
+	mmu.Write(addr, byte(val))
+	mmu.Write(addr+1, byte(val>>8))
+}
+
+func (mmu *MMU) mmapLocation(addr uint16) (buffer []byte, offset uint16) {
+	switch addr & 0xF000 {
+	// BIOS is mapped over ROM on startup
+	case 0x0000:
+		if mmu.biosEnable && addr < 0x0100 {
+			return mmu.bios, addr
+		}
+		fallthrough
+	case 0x1000:
+		fallthrough
+	case 0x2000:
+		fallthrough
+	case 0x3000:
+		fallthrough
+	case 0x4000:
+		fallthrough
+	case 0x5000:
+		fallthrough
+	case 0x6000:
+		fallthrough
+	case 0x7000:
+		return mmu.rom, addr
+	case 0x8000:
+		fallthrough
+	case 0x9000:
+		return mmu.vram, addr & 0x1FFF
+	case 0xA000:
+		fallthrough
+	case 0xB000:
+		return mmu.eram, addr & 0x1FFF
+	case 0xC000:
+		fallthrough
+	case 0xD000:
+		return mmu.wram, addr & 0x1FFF
+	// WRAM Shadow
+	case 0xE000:
+		return mmu.wram, addr & 0x1FFF
+	// Shadow, IO, and ZRAM
+	case 0xF000:
+		switch addr & 0x0F00 {
+		case 0x000:
+			fallthrough
+		case 0x100:
+			fallthrough
+		case 0x200:
+			fallthrough
+		case 0x300:
+			fallthrough
+		case 0x400:
+			fallthrough
+		case 0x500:
+			fallthrough
+		case 0x600:
+			fallthrough
+		case 0x700:
+			fallthrough
+		case 0x800:
+			fallthrough
+		case 0x900:
+			fallthrough
+		case 0xA00:
+			fallthrough
+		case 0xB00:
+			fallthrough
+		case 0xC00:
+			fallthrough
+		case 0xD00:
+			return mmu.wram, addr & 0x1FFF
+		// GPU OAM
+		case 0xE00:
+			if addr < 0xFEA0 {
+				return mmu.goam, addr & 0xFF
+			}
+			// Higher addresses always 0
+			return mmu.zero, 0
+		// Zpage & I/O
+		case 0xF00:
+			// ZPAGE
+			if addr >= 0xFF80 {
+				return mmu.zram, addr & 0x7F
+			}
+			// I/O
+			return mmu.zero, 0
+		}
+	}
+
+	log.Printf("Encountered unmapped memory location: %#4x", addr)
+	return mmu.zero, 0
 }
---
+++ internal/app/gba/mmu_test.go
@@ -0,0 +1,139 @@
+package gba
+
+import "testing"
+
+func TestMMUInit(t *testing.T) {
+	mmu := NewMMU()
+
+	if mmu.biosEnable != true {
+		t.Error("MMU biosEnable flag should init to true")
+	}
+}
+
+func TestMMUReadWrite(t *testing.T) {
+	mmu := NewMMU()
+
+	got := mmu.Read(0)
+	if got != 0 {
+		t.Errorf("Expected initial value to be 0, got %d", got)
+	}
+
+	mmu.Write(0, 0x12)
+	mmu.Write(1, 0x34)
+	mmu.Write16(2, 0x7856)
+
+	got = mmu.Read(0)
+	if got != 0x12 {
+		t.Errorf("Expected to read written value of 0x12, got %#2x", got)
+	}
+	got16 := mmu.Read16(1)
+	if got16 != 0x5634 {
+		t.Errorf("Expected to read written value of 0x5634, got %#4x", got16)
+	}
+	got = mmu.Read(3)
+	if got != 0x78 {
+		t.Errorf("Expected to read writte nvalue of 0x78, got %#2x", got)
+	}
+}
+
+func TestMMUBios(t *testing.T) {
+	mmu := NewMMU()
+
+	mmu.Write(0x099, 0x12)
+	mmu.Write(0x100, 0x34)
+
+	got := mmu.Read(0x099)
+	if got != 0x12 {
+		t.Errorf("Expected to read written value of 0x12, got %#2x", got)
+	}
+	got = mmu.Read(0x100)
+	if got != 0x34 {
+		t.Errorf("Expected to read written value of 0x34, got %#2x", got)
+	}
+
+	mmu.DisableBios()
+
+	got = mmu.Read(0x099)
+	if got != 0 {
+		t.Errorf("Expected to read 0 after disabling bios, got %#2x", got)
+	}
+	got = mmu.Read(0x100)
+	if got != 0x34 {
+		t.Errorf("Expected value 0x34 to be preserved after disabling bios, got %#2x", got)
+	}
+}
+
+func TestMMUMappings(t *testing.T) {
+	mmu := NewMMU()
+
+	mmu.Write(0x0000, 0x1) // BIOS
+	mmu.Write(0x0099, 0x2) // BIOS
+	mmu.Write(0x0100, 0x3) // ROM
+	mmu.Write(0x7FFF, 0x4) // ROM
+	mmu.Write(0x8000, 0x5) // VRAM
+	mmu.Write(0x9FFF, 0x6) // VRAM
+	mmu.Write(0xA000, 0x7) // ERAM
+	mmu.Write(0xBFFF, 0x8) // ERAM
+	mmu.Write(0xC000, 0x9) // WRAM
+	mmu.Write(0xDFFF, 0xA) // WRAM
+	mmu.Write(0xFE00, 0xB) // GOAM
+	mmu.Write(0xFF80, 0xC) // ZRAM
+	mmu.Write(0xFFFF, 0xD) // ZRAM
+
+	if mmu.bios[0x0000] != 0x1 {
+		t.Errorf("Expected to read 0x1 from bios memory, got %#2x", mmu.bios[0x0000])
+	}
+	if mmu.bios[0x0099] != 0x2 {
+		t.Errorf("Expected to read 0x2 from bios memory, got %#2x", mmu.bios[0x0099])
+	}
+	if mmu.rom[0x0100] != 0x3 {
+		t.Errorf("Expected to read 0x3 from rom, got %#2x", mmu.rom[0x0100])
+	}
+	if mmu.rom[0x7FFF] != 0x4 {
+		t.Errorf("Expected to read 0x4 from rom, got %#2x", mmu.rom[0x7FFF])
+	}
+	if mmu.vram[0x0000] != 0x5 {
+		t.Errorf("Expected to read 0x5 from vram, got %#2x", mmu.vram[0x0000])
+	}
+	if mmu.vram[0x1FFF] != 0x6 {
+		t.Errorf("Expected to read 0x6 from vram, got %#2x", mmu.vram[0x1FFF])
+	}
+	if mmu.eram[0x0000] != 0x7 {
+		t.Errorf("Expected to read 0x7 from eram, got %#2x", mmu.eram[0x0000])
+	}
+	if mmu.eram[0x1FFF] != 0x8 {
+		t.Errorf("Expected to read 0x8 from eram, got %#2x", mmu.eram[0x1FFF])
+	}
+	if mmu.wram[0x0000] != 0x9 {
+		t.Errorf("Expected to read 0x9 from wram, got %#2x", mmu.wram[0x0000])
+	}
+	if mmu.wram[0x1FFF] != 0xA {
+		t.Errorf("Expected to read 0xA from wram, got %#2x", mmu.wram[0x1FFF])
+	}
+	if mmu.goam[0x0000] != 0xB {
+		t.Errorf("Expected to read 0xB from goam, got %#2x", mmu.goam[0x0000])
+	}
+	if mmu.zram[0x0000] != 0xC {
+		t.Errorf("Expected to read 0xC from zram, got %#2x", mmu.zram[0x0000])
+	}
+	if mmu.zram[0x007F] != 0xD {
+		t.Errorf("Expected to read 0xD from zram, got %#2x", mmu.zram[0x007F])
+	}
+
+	if mmu.Read(0xE000) != 0x9 {
+		t.Errorf("Expected to read 0x9 from wram shadow, got %#2x", mmu.Read(0xE000))
+	}
+	if mmu.Read(0xF000) != 0x0 {
+		t.Errorf("Expected to read 0x0 from wram shadow, got %#2x", mmu.Read(0xF000))
+	}
+}
+
+func TestMMUZeros(t *testing.T) {
+	mmu := NewMMU()
+
+	mmu.Write(0xFEFF, 0xFF) // Should be forced to 0
+
+	if mmu.Read(0xFEFF) != 0 {
+		t.Errorf("Expected to read 0, got %#2x", mmu.Read(0xFEFF))
+	}
+}
